e070ef2b0310473759dcc4642769bf41aecdae48
Secaly <kevin.ledieu.fr@gmail.com>
2017-11-19 01:33:21 +0100

    redux setup

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -15,7 +15,7 @@ class LoginForm extends React.Component {
 
   onChange = e =>
     this.setState({
-      data: {...this.state.data, [e.target.name]: e.target.value }
+      data: { ...this.state.data, [e.target.name]: e.target.value }
     });
 
   onSubmit = () => {
